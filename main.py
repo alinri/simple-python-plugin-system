27089616174414a48347ef9897253327b9dc6082
SeyedMohammadAli Noori <13noori79@gmail.com>
2022-11-15 01:50:17 +0330

    Use better names for main.py variables

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from plugin_abstract import PlugIn
 
 
 def load_mods(text: str):
-    mods = [x.split('.')[0]for x in os.listdir(pathlib.Path() / 'mods')]
-    for mod_name in mods:
-        mod = importlib.import_module(f'mods.{mod_name}')
+    modules = [x.split('.')[0]for x in os.listdir(pathlib.Path() / 'mods')]
+    for module_name in modules:
+        mod = importlib.import_module(f'mods.{module_name}')
         instances = []
-        for _, obj in inspect.getmembers(mod, inspect.isclass):
-            if obj.__module__.split('.')[-1] == mod_name and issubclass(obj, PlugIn):
-                instances.append(obj())
+        for _, _cls in inspect.getmembers(mod, inspect.isclass):
+            if _cls.__module__.split('.')[-1] == module_name and issubclass(_cls, PlugIn):
+                instances.append(_cls())
 
         for plugin in instances:
             text = plugin.process_text(text)
